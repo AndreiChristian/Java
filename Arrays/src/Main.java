85d8d8013dacab72e2c71da2f7e66cd83397003b
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-07-17 19:43:39 +0200

    some work with array lists

--- Arrays/src/Main.java
+++ Arrays/src/Main.java
@@ -1,5 +1,58 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String[] cars = {"Camaro", "Corvette", "Mustang"};
+
+//            cars[4] = "Tesla";
+
+        for (int i = 0; i < cars.length; i++) {
+
+            System.out.println(cars[i]);
+
+        }
+
+        String name = "Andrei";
+
+        boolean isEqual = name.equals("Andre");
+
+        System.out.println(isEqual);
+
+        name.toLowerCase();
+        name.toUpperCase();
+
+        int charAtIndex = name.indexOf(3);
+
+        ArrayList<String> dogs;
+
+        dogs = new ArrayList<String>();
+
+        dogs.add("Beagle");
+        dogs.add("German Sh");
+        dogs.add("German Separd");
+        dogs.add("Germhepard");
+
+        dogs.set(3,"Bulldog");
+
+        dogs.clear();
+
+        for( int i=0; i< dogs.size(); i++ ) {
+
+            System.out.println(dogs.get(i));
+
+        }
+
+        String[] animals = {"cat","dog","rat","bird"};
+
+        for (String i:animals){
+
+            System.out.println(i);
+
+        }
+
+        
+
     }
 }
